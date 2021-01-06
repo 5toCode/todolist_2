c44b691a2e0c89a96b1aef5eb118ec10a9d9ffc5
mrobertsjr <70207997+mrobertsjr@users.noreply.github.com>
2021-01-06 07:01:37 -0500

    add ejs templates and module exports

--- app.js
+++ app.js
@@ -1,28 +1,35 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + '/date.js');
 
 const app = express();
 
-let newListItems = ['Buy food', 'Cook food', 'Eat food'];
+const items = ['Buy food', 'Cook food', 'Eat food'];
+const workItems = [];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
-const options = { weekday: 'long', month: 'long', day: 'numeric' };
-const today  = new Date();
-
-let day = today.toLocaleDateString("en-US", options)
-
 app.get("/", function(req, res){
-  res.render('list', {day: day, newListItems: newListItems});
+  const day = date.getDate();
+  res.render('list', {listTitle: day, newListItems: items});
 });
 
 app.post('/', function(req, res) {
-  let newListItem = req.body.newItem;
-  newListItems.push(newListItem);
+  const item = req.body.newItem;
+  if (req.body.list === 'Work') {
+    workItems.push(item);
+    res.redirect('/work');
+  } else {
+    items.push(item);
+  }
   res.redirect('/');
+});
+
+app.get('/work', function(req, res) {
+  res.render('list', {listTitle: 'Work List', newListItems: workItems})
 });
 
 app.listen(process.env.PORT || 3000, function(){
