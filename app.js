01380a0e745d32e0c61b8025eb84370e4e33e85c
mrobertsjr <70207997+mrobertsjr@users.noreply.github.com>
2021-01-12 17:44:36 -0500

    add monogdb functionality

--- app.js
+++ app.js
@@ -1,30 +1,65 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + '/date.js');
+const mongoose = require('mongoose');
+// const date = require(__dirname + '/date.js');
 
 const app = express();
-
-const items = ['Buy food', 'Cook food', 'Eat food'];
-const workItems = [];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
+mongoose.connect('mongodb://localhost:27017/todolistDB', {useNewUrlParser: true, useUnifiedTopology: true });
+
+const itemsSchema = new mongoose.Schema ({
+  name: String
+});
+
+const Item = mongoose.model('Item', itemsSchema);
+
+const item1 = new Item ({
+  name: 'Welcome to your todo list'
+});
+
+const item2 = new Item ({
+  name: 'Hit the + button to add a new item'
+});
+
+const item3 = new Item ({
+  name: '<-- Hit this to delete an item'
+});
+
+const defaultItems = [item1, item2, item3];
+
 app.get("/", function(req, res){
-  const day = date.getDate();
-  res.render('list', {listTitle: day, newListItems: items});
+  // const day = date.getDate();
+  Item.find({}, function(err, foundItems) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundItems.length === 0) {
+        Item.insertMany(defaultItems, function(err) {
+          if (err) {
+            console.log(err)
+          } else {
+            console.log('Successfully inserted items!')
+            res.redirect('/');
+          }
+        });
+      } else {
+        res.render('list', {listTitle: 'Today', newListItems: foundItems});
+      }
+    }
+  });
 });
 
 app.post('/', function(req, res) {
-  const item = req.body.newItem;
-  if (req.body.list === 'Work') {
-    workItems.push(item);
-    res.redirect('/work');
-  } else {
-    items.push(item);
-  }
+  const itemName = req.body.newItem;
+  const item = new Item ({
+    name: itemName
+  });
+  item.save();
   res.redirect('/');
 });
 
