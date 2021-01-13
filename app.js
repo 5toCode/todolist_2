875838eb8deaf52272daa1ef26f4ba6ddb96dda7
mrobertsjr <70207997+mrobertsjr@users.noreply.github.com>
2021-01-13 06:44:51 -0500

    change to MongoDB Atlas

--- app.js
+++ app.js
@@ -10,7 +10,13 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
-mongoose.connect('mongodb://localhost:27017/todolistDB', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+const dburl = process.env.DATABASEURL || 'mongodb://localhost:27017/todolistDB'
+
+mongoose.connect(dburl, {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true, 
+  useFindAndModify: false 
+});
 
 const itemsSchema = new mongoose.Schema ({
   name: String
